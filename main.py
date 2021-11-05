679ffa987a65ccc5e1e467c2651c7c4b4cde94bf
Connor Watson <connorwatson0811@gmail.com>
2021-11-04 22:12:58 -0400

    20211013 - Added pdftotext raw vs physical page parsing

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import logging
 def parse_arguments(**kwargs):
     parser = argparse.ArgumentParser(description='This script tries to read in a PDF statement.')
     # parser.add_argument('path', help='Absolute\\Path\\To\\Statement.pdf')
+    parser.add_argument('-ra', '--raw', help='Store PDF pages in order it appears in content stream via pdftotext',
+                        action='store_true', default=False)
+    parser.add_argument('-ph', '--phy', help='Store PDF pages in order it appears regardless of format via pdftotext',
+                        action='store_true', default=False)
     parser.add_argument('-vb', '--verbose', help='Turn on verbose debugging', action='store_true',
                         default=False)
     parser.add_argument('statement', help='Absolute\\Path\\To\\Statement.pdf')
@@ -33,10 +37,14 @@ def parse_arguments(**kwargs):
 def main():
     cmd_args = parse_arguments(verbose_logs=True)
     logging.debug('Hello world!')
-    statement_parser = process_statement.ProcessStatement(cmd_args.statement)
+    statement_parser = process_statement.ProcessStatement(cmd_args.statement, raw=cmd_args.raw, physical=cmd_args.phy)
     statement_parser.read_pdf_file()
     statement_parser.get_page_numbers()
-    statement_parser.print_pdf_page(3)
+    p_num = 3
+    #statement_parser.print_pdf_page(p_num)
+    # statement_parser.pretty_print_page(3)
+    #print(help(pdftotext))
+    statement_parser.write_page_to_txt_file(p_num, f'C:\\Users\\watson\\Documents\\DataSets\\page_{p_num}.txt')
 
 
 if __name__ == '__main__':
--- process_statement.py
+++ process_statement.py
@@ -8,15 +8,17 @@ class ProcessStatement:
 
     """This class contains all the processing functions for one PDF statement (specific to one particular bank)"""
 
-    def __init__(self, pdfpath):
+    def __init__(self, pdfpath, **kwargs):
         logging.debug('Inside Process Statement')
         self.pdfpath = pdfpath
         self.pages = {}
+        self.pdf_raw_output = kwargs.get('raw', False)
+        self.pdf_phy_output = kwargs.get('physical', False)
 
     def read_pdf_file(self):
         try:
             with open(self.pdfpath, 'rb') as f:
-                pdf = pdftotext.PDF(f)
+                pdf = pdftotext.PDF(f, raw=self.pdf_raw_output, physical=self.pdf_phy_output)
         except FileNotFoundError as fe:
             print('File not found with path: ' + self.pdfpath)
             sys.exit(0)
@@ -30,6 +32,17 @@ class ProcessStatement:
     def print_pdf_page(self, pagenum):
         print(self.pages[pagenum])
 
+    def pretty_print_page(self, pagenum):
+        for line in self.pages[pagenum].split('\n'):
+            print(line)
+
     def get_page_numbers(self):
         for pagenum in sorted(self.pages.keys()):
             print('Page {}/{}'.format(pagenum, len(self.pages.keys())))
+
+    def get_pdf_page(self, pagenum):
+        return(self.pages[pagenum])
+
+    def write_page_to_txt_file(self, pagenum, out_file):
+        with open(out_file, 'w') as of:
+            of.write(self.get_pdf_page(pagenum))
