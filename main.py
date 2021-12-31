04e4aab215fb61cd4d0947a04ddb08392d71ad9c
Connor Watson <connorwatson0811@gmail.com>
2021-12-31 13:08:10 -0500

    20211231 - finished configuring logging object for all modules

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import logging
 import os
 import sys
 import pathlib
+logger = logging.getLogger('main_logger')
 
 
 def parse_arguments(): # **kwargs
+    global logger
     parser = argparse.ArgumentParser(description='This script tries to read in a PDF statement.')
     # parser.add_argument('-pn', '--page', help='Read just one page', default=-1, type=int)
     # parser.add_argument('-ra', '--raw', help='Store PDF pages in order it appears in content stream via pdftotext',
@@ -28,7 +30,7 @@ def parse_arguments(): # **kwargs
     except Exception as e:
         print(str(e))
         sys.exit(1)
-    log_fmt = '%(asctime)s LINE:%(lineno)d LEVEL:%(levelname)s %(message)s'
+    log_fmt = '%(asctime)s LINE:%(lineno)d %(module)s LEVEL:%(levelname)s %(message)s'
     verbose_logs = config['verbose_logging']
     if verbose_logs:
         log_lvl = logging.DEBUG
@@ -42,6 +44,10 @@ def parse_arguments(): # **kwargs
     else:
         logging.basicConfig(level=log_lvl, format=log_fmt)
     '''
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(log_lvl)
+    stream_handler.setFormatter(logging.Formatter(log_fmt))
+    logger.addHandler(stream_handler)
     return args
 
 """
@@ -66,13 +72,12 @@ another funciton with error checking
 """
 
 
-
 def main():
     in_args = parse_arguments()
     config = json.load(open(in_args.config))
     p_num_check = int(config['page_numbers_to_read'])
     out_dir = config['output_directory']
-    logging.debug(f'Output directory is: {out_dir}')
+    logger.debug(f'Output directory is: {out_dir}')
     if not os.path.exists(config['output_directory']):
         os.mkdir(config['output_directory'])
     for filepath in pathlib.Path(config['statements_path']).glob('**/*'):
@@ -83,19 +88,20 @@ def main():
         statement_parser = process_statement.ProcessStatement(statement_path,
                                                               raw=config['raw_pdf_content'],
                                                               physical=config['physical_pdf_content'])
-        logging.debug(statement_path)
+        logger.debug(statement_path)
         statement_parser.read_pdf_file()
         page_numbers = statement_parser.get_page_numbers()
         if p_num_check > 0:
-            logging.debug(f'Processing page #{p_num_check} only')
+            logger.debug(f'Processing page #{p_num_check} only')
             output_file_name = os.path.abspath(os.path.join(out_dir, f'{fname}_page_{p_num_check}.txt'))
-            logging.debug(f'Writing page #{p_num_check} only to {output_file_name}')
+            logger.debug(f'Writing page #{p_num_check} only to {output_file_name}')
             statement_parser.write_page_to_txt_file(p_num_check, output_file_name)
+            statement_parser.process_pdf_page(p_num_check)
         else:
-            logging.debug(f'Will process all pages of the document')
+            logger.debug(f'Will process all pages of the document')
             for page_num in page_numbers:
                 output_file_name = os.path.abspath(os.path.join(out_dir, f'{fname}_page_{page_num}.txt'))
-                logging.debug(f'Writing page #{page_num} to {output_file_name}')
+                logger.debug(f'Writing page #{page_num} to {output_file_name}')
                 statement_parser.write_page_to_txt_file(page_num, output_file_name)
 
 
--- process_statement.py
+++ process_statement.py
@@ -1,7 +1,8 @@
 import logging
 import sys
-
+import re
 import pdftotext
+# logging.getLogger('statement_logging')
 
 
 class ProcessStatement:
@@ -19,32 +20,55 @@ class ProcessStatement:
             with open(self.pdfpath, 'rb') as f:
                 pdf = pdftotext.PDF(f, raw=self.pdf_raw_output, physical=self.pdf_phy_output)
         except FileNotFoundError as fe:
-            print('File not found with path: ' + self.pdfpath)
+            logging.error('File not found with path: ' + self.pdfpath)
             sys.exit(0)
         except Exception as e:
-            print('Unexpected error occured!')
-            print(e)
+            logging.error('Unexpected error occured!')
+            logging.error(e)
             sys.exit(0)
         for pagenum in range(len(pdf)):
             self.pages[pagenum + 1] = pdf[pagenum]
 
     def print_pdf_page(self, pagenum):
-        print(self.pages[pagenum])
+        logging.info(self.pages[pagenum])
 
     def pretty_print_page(self, pagenum):
         for line in self.pages[pagenum].split('\n'):
-            print(line)
+            logging.info(line)
 
     def get_page_numbers(self):
         return list(sorted(self.pages.keys()))
 
     def print_page_numbers(self):
         for pagenum in sorted(self.pages.keys()):
-            print('Page {}/{}'.format(pagenum, len(self.pages.keys())))
+            logging.info('Page {}/{}'.format(pagenum, len(self.pages.keys())))
 
     def get_pdf_page(self, pagenum):
-        return(self.pages[pagenum])
+        return self.pages[pagenum]
 
     def write_page_to_txt_file(self, pagenum, out_file):
         with open(out_file, 'w') as of:
             of.write(self.get_pdf_page(pagenum))
+
+    def process_pdf_page(self, page_num):
+        num_line_with_transactions = 0
+        for line in self.get_pdf_page(page_num).split('\n'):
+            try:
+                int(line[0])
+            except ValueError as ve:
+                continue
+            except Exception as e:
+                continue
+            if re.search("[0-9][0-9]/[0-9][0-9]/[0-9][0-9]", line[:8]):
+                num_line_with_transactions += 1
+                transaction_date = line[:8]
+                line_temp = line[8:]
+                logging.debug(line_temp)
+                transaction_regex = r"-?([0-9]+,?)+\.[0-9][0-9]"
+                transaction_amount = re.search(transaction_regex, line_temp).group()
+                transaction_description = re.sub(transaction_regex, "", line_temp).strip()
+                logging.debug(f'{transaction_date} | {transaction_amount} | {transaction_description}')
+        logging.info(f'Total {num_line_with_transactions} lines with transactions')
+
+
+
